55f6625790e2d4e3d47ad76d8ee66c3a6e6fb9a5
Saiful Islam Reyad <riyadsaif18@gmail.com>
2024-10-21 09:38:45 +0600

    first commit

---
+++ book/book/spiders/bookspider.py
@@ -0,0 +1,18 @@
+import scrapy
+
+
+class BookspiderSpider(scrapy.Spider):
+    name = "bookspider"
+    allowed_domains = ["books.toscrape.com"]
+    start_urls = ["https://books.toscrape.com/"]
+
+    def parse(self, response):
+            for book in response.css('li.col-xs-6'):
+                yield {
+                    'title': book.css('h3 a::text').get(),
+                    'link': response.urljoin(book.css('h3 a::attr(href)').get()),
+                    'price': book.css('p.price_color::text').get()
+                }
+            next_page = response.css('li.next a::attr(href)').get()
+            if next_page:
+                yield response.follow(next_page, self.parse)
